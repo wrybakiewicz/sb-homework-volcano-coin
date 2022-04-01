cd5b433894872da2e6a032181a2949daa2fd4318
Wojciech Rybakiewicz <wojtek.rybakiewicz@gmail.com>
2022-04-01 17:46:01 +0200

    get rid of console.log

--- contracts/VolcanoCoin.sol
+++ contracts/VolcanoCoin.sol
@@ -1,8 +1,6 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
-import "hardhat/console.sol";
-
 contract VolcanoCoin {
     uint public totalSupply = 10_000;
     address public owner;
